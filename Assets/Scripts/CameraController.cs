59cc0526aee7a643718466c64d07d7c933d32373
jellowman <tarfisher@gmail.com>
2015-10-30 23:06:52 -0500

    Did a camera thing

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,14 +4,23 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 
     public GameObject player;
+    public float cameraDistance;
     private Vector3 offset;
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
+    void Update ()
+    {
+        offset = player.GetComponent<Rigidbody>().GetPointVelocity(player.transform.position);
+        offset.Normalize();
+        offset *= (6 + player.GetComponent<Rigidbody>().GetPointVelocity(player.transform.position).magnitude);
+        offset *= -1;
+    }
 	void LateUpdate () {
         transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position, Vector3.up);
 	}
 }
