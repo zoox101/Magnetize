4b901e27aae2779d8cdf0c892ad2eb13cb220bda
zoox101 <william@thebookers.net>
2015-10-31 13:46:51 -0500

    Added force trap.

---
+++ Assets/Scripts/Force.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Force : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player")) 
+		{
+			int kickHigh = 2000;
+			int kickLow = 1500;
+
+			Rigidbody player = other.GetComponent<Rigidbody>();
+
+			int[] comp = new int[3];
+			for(int i=0; i<comp.Length; i++)
+			{
+				comp[i] =  Random.Range(kickLow, kickHigh);
+				if(Random.value < 0.5f) comp[i] *= -1;
+			}
+
+			Vector3 force = new Vector3(comp[0], comp[1], comp[2]);
+			player.AddForce(force);
+			Debug.Log(force);
+			this.gameObject.SetActive(false);
+		}
+	}
+}
