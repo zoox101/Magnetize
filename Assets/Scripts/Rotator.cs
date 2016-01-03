faa7b430ea94dc48a98e29ca9310bd074e1794b3
zoox101 <william@thebookers.net>
2016-01-03 00:29:10 -0600

    Made the intro screen look fancy.

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,9 +4,10 @@ using System.Collections;
 public class Rotator : MonoBehaviour 
 {
 	public int direction = 1;
+	public float speed = 1;
 
 	void Update () 
     {
-		transform.Rotate (new Vector3(15 * direction, 30 * direction, 45 * direction) * Time.deltaTime);	
+		transform.Rotate (new Vector3(speed * 15 * direction, speed * 30 * direction, speed * 45 * direction) * Time.deltaTime);	
 	}
 }
---
+++ Assets/Scripts/TitleScreenCamera.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenCamera : MonoBehaviour 
+{
+	private int cameraDistance = 20;
+	public GameObject reference;
+	private Transform targetLocation;
+
+	void Start () 
+	{
+		targetLocation = reference.transform;
+	}
+	
+	void Update () 
+	{
+		//transform.Rotate (new Vector3(1 * 15 * direction, speed * 30 * direction, speed * 45 * direction) * Time.deltaTime);
+		transform.LookAt (targetLocation.position);
+
+		Quaternion dim4 = targetLocation.rotation;
+		Vector3 dim3 = dim4.eulerAngles;
+
+		//Quaternion dim4 = targetLocation.rotation;
+		//Vector3 dim3 = dim4.eulerAngles;
+		//Vector3 newPos = targetLocation.position + cameraDistance * dim3;
+		//transform.position = newPos;
+	}
+}
